e6b2fa00ce19d780e411cc2b5930c84d715dbbe6
Pierre-François Salmand <maabaa@gmail.com>
2022-08-22 01:42:20 +0700

    Finish n.1

--- app/2.2 DOM modification/DOM_modification_script.js
+++ app/2.2 DOM modification/DOM_modification_script.js
@@ -1,15 +1,25 @@
 /* eslint-disable no-console */
+/* eslint-disable no-restricted-syntax */
 
 console.log("~~~~~~~~~~ 2.2 ~~~~~~~~~~\n ");
 
-// console.dir(document.querySelectorAll("*"));
 
-console.log("\n1. Écris une fonction changeTitles()");
+// 1. Write a changeText function
+const changeText = (currentText, replacementText, element = "*") => {
+  const allNodes = document.body.querySelectorAll(element);
 
-const changeTitles = () => {
-  const allNodes = document.body.querySelectorAll("*");
-  console.dir(allNodes);
-  
-}
+  for (const node of allNodes) {
+    if (node.textContent.startsWith(currentText)) {
+      node.innerText = replacementText;
+    }
+  }
+};
 
-changeTitles();
+changeText("Album example", "Ce que j'ai appris à THP", "h1");
+
+changeText(
+  "Something short and leading about",
+  "THP est une formation qui, en 3 mois, à plein temps, vous apportera des connaissances actionnables pour vous permettre de voir plus loin. Chez nous, pas de professeurs, pas de locaux, mais un groupe de travail en présentiel. Après 11 semaines, les principaux langages et outils du web n'auront plus de secret pour vous !"
+);
+
+
