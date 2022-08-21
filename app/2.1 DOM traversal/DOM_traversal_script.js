00286bc0e1d2f001946f12e575340d5d185d4f1a
Pierre-François Salmand <maabaa@gmail.com>
2022-08-22 01:41:44 +0700

    Add missing exercises to 2.1

--- app/2.1 DOM traversal/DOM_traversal_script.js
+++ app/2.1 DOM traversal/DOM_traversal_script.js
@@ -20,8 +20,16 @@ console.dir(document.querySelectorAll("a")[2].attributes.href.value);
 console.log("\n4. Combien d'éléments portent la classe compte-moi ?");
 console.dir(document.querySelectorAll(".compte-moi").length);
 
+console.log("\n5. Combien d'éléments <li> portent la classe compte-moi ?");
+console.dir(document.querySelectorAll("li.compte-moi").length);
+
 console.log(
-  '\n5. La page contient un seul élément <div>. Celui-ci contient 2 éléments "unordered list" ou <ul>. Dans le second <ul>, le premier élément de la liste (tag <li>) est caché visuellement de l\'utilisateur:'
+  "\n6. Combien d'éléments <li> et situés dans une liste ordonnée portent la classe compte-moi ?"
+);
+console.dir(document.querySelectorAll("ol > li.compte-moi").length);
+
+console.log(
+  '\n7. La page contient un seul élément <div>. Celui-ci contient 2 éléments "unordered list" ou <ul>. Dans le second <ul>, le premier élément de la liste (tag <li>) est caché visuellement de l\'utilisateur:'
 );
 console.dir(
   document.querySelector("div").querySelectorAll("ul")[1].querySelector("li")
